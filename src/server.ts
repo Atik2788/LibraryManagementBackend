f33b334b5054fdf8a2941208d6f6bab7421117f4
Atik2788 <ashanto653.2@gmail.com>
2025-08-15 23:49:28 +0600

    update origin 5000 to 5173

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(cors(
     {
-    origin: ['http://localhost:5000', 'https://librarymanagementbackend-production-2084.up.railway.app/']
+    origin: ['http://localhost:5173', 'https://librarymanagementbackend-production-2084.up.railway.app/']
    }
 ))
 app.use(express.json())
